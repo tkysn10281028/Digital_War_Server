0ae927f61340bb3cba63af4311437573149c86dc
tkysn10281028 <95330554+tkysn10281028@users.noreply.github.com>
2025-10-20 21:33:54 +0900

    テスト用クライアントの作成とunary Projectの実装

---
+++ realtime-client-test/realtime-client-unary/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using MagicOnion;
+using MagicOnion.Client;
+using Shared.IFs;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+
+        // IChatService
+        var response = await PrepareMagicOnionClient<IChatService>(channel).SendMessageAsync("Alice", "message");
+        Console.WriteLine($"Executed IChatService. Response : {response}");
+    }
+
+    private static T PrepareMagicOnionClient<T>(GrpcChannel channel) where T : IService<T>
+    {
+        return MagicOnionClient.Create<T>(channel);
+    }
+}
