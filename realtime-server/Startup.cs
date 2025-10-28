1145fb1f33749f0a7d0b71b266bf220f6004a201
tkysn10281028 <95330554+tkysn10281028@users.noreply.github.com>
2025-10-28 22:14:13 +0900

    複数サーバー構成をRedisで対応できるように修正

--- realtime-server/Startup.cs
+++ realtime-server/Startup.cs
@@ -2,17 +2,24 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
 
 namespace RealtimeServer
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
-            services.AddMagicOnion();
+            var connectUrl = Environment.GetEnvironmentVariable("REDIS_HOST") != null ? "redis:6379" : "localhost:6379";
+            var redis = ConnectionMultiplexer.Connect(connectUrl);
+            services.AddSingleton<IConnectionMultiplexer>(redis);
+            services.AddMagicOnion().UseRedisGroup(options =>
+                {
+                    options.ConnectionMultiplexer = redis;
+                }, registerAsDefault: true);
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
