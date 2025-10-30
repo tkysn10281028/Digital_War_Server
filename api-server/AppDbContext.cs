479fa8395e925ff18b429884e5dce2ab087d44c8
tkysn10281028 <95330554+tkysn10281028@users.noreply.github.com>
2025-10-30 13:29:38 +0900

    マイグレ方法の修正

--- api-server/AppDbContext.cs
+++ api-server/AppDbContext.cs
@@ -6,5 +6,17 @@ namespace ApiServer
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         public DbSet<Test> Tests { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Test>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id)
+                      .ValueGeneratedOnAdd();
+                entity.Property(e => e.CreatedAt)
+                      .IsRequired();
+            });
+        }
     }
 }
--- api-server/Project/Models/Test.cs
+++ api-server/Project/Models/Test.cs
@@ -1,12 +1,7 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace ApiServer.Project.Models
 {
     public class Test
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
         public long CreatedAt { get; set; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
     }
