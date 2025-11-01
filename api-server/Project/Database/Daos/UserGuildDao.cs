28c367c1087e027b33c5af9a10fe3b687fb3e4b2
tkysn10281028 <95330554+tkysn10281028@users.noreply.github.com>
2025-11-01 12:03:39 +0900

    Dao,Logicを追加

---
+++ api-server/Project/Database/Daos/UserGuildDao.cs
@@ -0,0 +1,7 @@
+namespace ApiServer.Project.Database.Daos
+{
+    public class UserGuildDao
+    {
+
+    }
+}
---
+++ api-server/Project/Domains/Logics/UserLoginLogic.cs
@@ -0,0 +1,7 @@
+namespace ApiServer.Project.Domains.Logics
+{
+    public class UserLoginLogic
+    {
+
+    }
+}
